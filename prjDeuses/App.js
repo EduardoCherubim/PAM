c8699b35e1d62ecb920c1e6fc86b33206519f702
EduardoCherubim <142914224+EduardoCherubim@users.noreply.github.com>
2024-06-11 08:37:12 -0300

    Update App.js

--- prjDeuses/App.js
+++ prjDeuses/App.js
@@ -7,101 +7,102 @@ import { useState } from 'react';
 export default function App() {
   
 const [deus,setDeus] = useState([
-  {nome:'Zeus',
-  ocupação:'deus dos deuses',
-  LocalCulto:'Olímpia',
-  Morada:'Monte Olimpo',
-  Clã:'Olimpianos',
-  Cônjuge:'Hera',
-  Romano:'Júpiter'
+  {
+  nome:'Nome: Zeus',
+  ocupação:'Ocupação: deus dos deuses',
+  LocalCulto:'Local que é cultuado:Olímpia',
+  Morada:'Morada: Monte Olimpo',
+  Clã:'Clã: Olimpianos',
+  Cônjuge:'Cônjuge: Hera',
+  Romano:'Nome romano: Júpiter'
   },
   {nome:'Poseidon', 
-  ocupação:'deus dos mares',
-  LocalCulto:'Pilos',
-  Morada:'Mar',
-  Clã:'Olimpianos',
-  Cônjuge:'Anifrite',
-  Romano:'Netuno'
+  ocupação:'Ocupação: deus dos mares',
+  LocalCulto:'Local que é cultuado: Pilos',
+  Morada:'Morada: Mar',
+  Clã:'Clã: Olimpianos',
+  Cônjuge:'Cônjuge: Anifrite',
+  Romano:'Nome romano: Netuno'
   },
   {nome:'Atena',
   ocupação:'deusa da saedoria',
   LocalCulto:'Atenas',
   Morada:'Monte Olimpo',
   Clã:'Olimpianos',
-  Cônjuge:'Não teve',
-  Romano:Minerva''
+  Cônjuge:'Cônjuge: Não teve',
+  Romano:'Nome romano: Minerva'
   },
   {nome:'Ares',
   ocupação:'deus da guerra',
    LocalCulto:'Tessália',
   Morada:'Monte Olimpo',
   Clã:'Olimpianos',
-  Cônjuge:'Não teve',
-  Romano:'Marte'
+  Cônjuge:'Cônjuge: Não teve',
+  Romano:'Nome romano: Marte'
   },
   {nome:'Ártemis',
   ocupação:'deusa da caça',
    LocalCulto:'Delos',
   Morada:'Monte Olimpo',
   Clã:'Olimpianos',
-  Cônjuge:'Não teve',
-  Romano:'Diana'
+  Cônjuge:'Cônjuge: Não teve',
+  Romano:'Nome romano: Diana'
   },
   {nome:'Hermes',
   ocupação:'deus das comunicações e das viagens',
    LocalCulto:'',
   Morada:'Monte Olimpo',
   Clã:'Olimpianos',
-  Cônjuge:'Não teve',
-  Romano:'Mercúrio'
+  Cônjuge:'Cônjuge: Não teve',
+  Romano:'Nome romano: Mercúrio'
   },
   {nome:'Hera',
   ocupação:'deusa dos céu,maternidade e matrimônio',
    LocalCulto:'Olímpia',
   Morada:'Monte Olimpo',
   Clã:'Olimpianos',
-  Cônjuge:'Zeus',
-  Romano:'Juno'
+  Cônjuge:'Cônjuge: Zeus',
+  Romano:'Nome romano: Juno'
   },
   {nome:'Deméter',
   ocupação:'deus da terra fértil',
    LocalCulto:'Festival mistérios de Elêusis',
   Morada:'Monte Olimpo',
   Clã:'Olimpianos',
-  Cônjuge:'Não teve',
-  Romano:Ceres''
+  Cônjuge:'Cônjuge: Não teve',
+  Romano:'Nome romano: Ceres'
   },
   {nome:'Hefesto',
   ocupação:'deus dos ferreiros',
    LocalCulto:'Lemnos',
   Morada:'Monte Olimpo',
   Clã:'Olimpianos',
-  Cônjuge:'Afrodite',
-  Romano:'Vulcano'
+  Cônjuge:'Cônjuge: Afrodite',
+  Romano:'Nome romano: Vulcano'
   },
   {nome:'Apolo',
   ocupação:'deus do Sol',
    LocalCulto:'Delfos',
   Morada:'Monte Olimpo',
   Clã:'Olimpianos',
-  Cônjuge:'Não teve',
-  Romano:'Febo'
+  Cônjuge:'Cônjuge: Não teve',
+  Romano:'Nome romano: Febo'
   },
   {nome:'Dionísio',
   ocupação:'deus da festa, do vinho e do prazer',
    LocalCulto:'Atenas',
   Morada:'Monte Olimpo',
   Clã:'Olimpianos',
-  Cônjuge:'Ariadne',
-  Romano:'Baco'
+  Cônjuge:'Cônjuge: Ariadne',
+  Romano:'Nome romano: Baco'
   },
   {nome:'Afrodite',
   ocupação:'deusa da beleza e do amor',
    LocalCulto:'Citera',
   Morada:'Monte Olimpo',
   Clã:'Olimpianos',
-  Cônjuge:'Hefesto',
-  Romano:'Vênus'
+  Cônjuge:'Cônjuge: Hefesto',
+  Romano:'Nome romano: Vênus'
   },
   ])
 
@@ -119,22 +120,47 @@ const [deus,setDeus] = useState([
     <View style={(styles.container)}>
       <ScrollView>
       <StatusBar style="auto" />
-      <Button title="Zeus" onPress={() => carregar (0)}/>
-      <Button title="Poseidon" onPress={() => carregar (1)}/>
-      <Button title="Atena" onPress={() => carregar (2)}/>
-      <Button title="Ares" onPress={() => carregar (3)}/>
-      <Button title="Ártemis" onPress={() => carregar (4)}/>
-      <Button title="Hermes" onPress={() => carregar (5)}/>
-      <Button title="Hera" onPress={() => carregar (6)}/>
-      <Button title="Deméter" onPress={() => carregar (7)}/>
-      <Button title="Hefesto" onPress={() => carregar (8)}/>
-      <Button title="Apolo" onPress={() => carregar (9)}/>
-      <Button title="Dionísio" onPress={() => carregar (10)}/>
-      <Button title="Afrodite" onPress={() => carregar (11)}/>
+      <View style={styles.botao}>
+      <Button style={(styles.btn)} title="Zeus" onPress={() => carregar (0)}/>
+      </View>
+      <View style={styles.botao}>
+      <Button style={(styles.btn)} title="Poseidon" onPress={() => carregar (1)}/>
+      </View>
+      <View style={styles.botao}>
+      <Button style={(styles.btn)} title="Atena" onPress={() => carregar (2)}/>
+      </View>
+      <View style={styles.botao}>
+      <Button style={(styles.btn)} title="Ares" onPress={() => carregar (3)}/>
+      </View>
+      <View style={styles.botao}>
+      <Button style={(styles.btn)} title="Ártemis" onPress={() => carregar (4)}/>
+      </View>
+      <View style={styles.botao}>
+      <Button style={(styles.btn)} title="Hermes" onPress={() => carregar (5)}/>
+      </View>
+      <View style={styles.botao}>
+      <Button style={(styles.btn)} title="Hera" onPress={() => carregar (6)}/>
+      </View>
+      <View style={styles.botao}>
+      <Button style={(styles.btn)} title="Deméter" onPress={() => carregar (7)}/>
+      </View>
+      <View style={styles.botao}>
+      <Button style={(styles.btn)} title="Hefesto" onPress={() => carregar (8)}/>
+      </View>
+      <View style={styles.botao}>
+      <Button style={(styles.btn)} title="Apolo" onPress={() => carregar (9)}/>
+      </View>
+      <View style={styles.botao}>
+      <Button style={(styles.btn)} title="Dionísio" onPress={() => carregar (10)}/>
+      </View>
+      <View style={styles.botao}>
+      <Button style={(styles.btn)} title="Afrodite" onPress={() => carregar (11)}/>
+      </View>
 
 
       <Modal visible={visivel} animationType="fade">
         <View>
+          <Text>{deus[indiceSelecionado.imagem]}</Text>
           <Text>{deus[indiceSelecionado].nome}</Text>
           <Text>{deus[indiceSelecionado].ocupação}</Text>
           <Text>{deus[indiceSelecionado].LocalCulto}</Text>
@@ -143,6 +169,7 @@ const [deus,setDeus] = useState([
           <Text>{deus[indiceSelecionado].Cônjuge}</Text>
           <Text>{deus[indiceSelecionado].Romano}</Text>
           <Button title="Voltar" onPress={() => voltar()}/>
+          <Image source={require} style={{width:50, height:50}}/>
         </View>
       </Modal>
 
@@ -158,4 +185,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  btn:{
+    justifyContent:'space-between',
+  },
+  botao:{
+justifyContent:'center',
+marginTop:40
+  },
+
 });
